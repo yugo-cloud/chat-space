6c3adcb17063aacbd57e2ceaba4b5a0a9eda2c0e
tetzng <ffffffffffffunk@gmail.com>
2019-08-17 00:57:07 +0900

    create users functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_pemitted_parameters, if: :devise_cotroller?
+
+  protected
+
+  def configure_pemitted_parameters
+    devise_parameter_sanitizer.permit (:sign_up, keys: [:name])
+  end
 end
