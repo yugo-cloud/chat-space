70384ea4f7519f4bdae68e816134174acf098edd
tetzng <ffffffffffffunk@gmail.com>
2019-08-19 21:07:53 +0900

    fixed message.js and create.json.jbuilder

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,22 +1,15 @@
 $(function(){
   function buildHTML(message){
-    if (message.content === undefined) {
-      var content = ""
-    } else {
-      var content = `<p class="lower-message__content">${message.content}</p>`
-    }
-    if (message.image === null) {
-      var image = ""
-    } else {
-      var image = `<img class="lower-message__image" src="${message.image}">`
-    }
+    var content = message.content ? `<p class="lower-message__content">${message.content}</p>` : "";
+    var image = message.image ? `<img class="lower-message__image" src="${message.image}">` : "";
+
     var html = `<div class="message">
                 <div class="upper-message">
                 <div class="upper-message__user-name">
                 ${message.user_name}
                 </div>
                 <div class="upper-message__date">
-                ${dateFormat(message.date)}
+                ${message.date}
                 </div>
                 </div>
                 <div class="lower-message">
@@ -25,28 +18,6 @@ $(function(){
                 </div>
                 </div>`
     return html;
-  }
-
-  function dateFormat(date) {
-    var date = new Date(date);
-    var y = date.getFullYear();
-    var m = date.getMonth() + 1;
-    var d = date.getDate();
-    var h = date.getHours();
-    var min = date.getMinutes();
-    if (m < 10) {
-      m = '0' + m;
-    }
-    if (d < 10) {
-      d = '0' + d;
-    }
-    if (h < 10) {
-      h = '0' + h;
-    }
-    if (min < 10) {
-      min = '0' + min;
-    }
-    return `${y}/${m}/${d} ${h}:${min}`;
   }
 
   $('#new_message').on('submit', function(e){
@@ -64,8 +35,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
-      $('#message_image').val('');
+      $('#new_message')[0].reset();
       var height = $('.messages')[0].scrollHeight;
       $('.messages').animate({scrollTop: height}, 500, 'swing');
     })
